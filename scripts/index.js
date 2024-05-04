73969c2a9d6c74e74b7fe369ca55c28f242cd025
Yulya Alymova <yumka2007@yandex.ru>
2024-05-04 12:15:16 +0300

    4_05_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 const cardTemplate = document.querySelector('#card-template').content; 
-const cardList = document.querySelector('.places__list'); 
+const cardList = document.querySelector('.places__list');  
 
-function addCard(arrayElementNumber, removeCard) {
+function addCard(arrayElement, removeCard) {
     const userCard = cardTemplate.querySelector('.places__item').cloneNode(true);
-    userCard.querySelector('.card__image').src = initialCards[arrayElementNumber].link;
-    userCard.querySelector('.card__title').textContent = initialCards[arrayElementNumber].name;
+    userCard.querySelector('.card__image').src = arrayElement.link;
+    userCard.querySelector('.card__image').alt = arrayElement.name;
+    userCard.querySelector('.card__title').textContent = arrayElement.name;
     userCard.querySelector('.card__delete-button').addEventListener('click', () => {removeCard(userCard)});
     return userCard;
 }    
@@ -13,7 +14,7 @@ function removeElement(item) {
     item.remove();
 }
 
-for (i = 0; i < initialCards.length; i++) {
-    const cardElement = addCard(i, removeElement);
+initialCards.forEach(element => {
+    const cardElement = addCard(element, removeElement);
     cardList.append(cardElement);
-}
+});
